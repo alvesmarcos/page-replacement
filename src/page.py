e6e367b890db6af53f716d72d0bcd7d2ad999460
Marcos Alves <alves.henrique.marcos@gmail.com>
2016-11-09 04:16:02 -0300

    Algorithm LRU implemented

--- src/page.py
+++ src/page.py
@@ -9,7 +9,7 @@ def fifo(queue):
 	# index circulate
 	index = 0
 	# get numbers frames
-	frames = [-1] * queue[0]
+	frames = [-1 for x in range(queue[0])]
 	queue.pop(0)
 	# swap pages
 	count = 0
@@ -24,9 +24,50 @@ def fifo(queue):
 	print("FIFO", count)
 
 # Least Recently Used 
-def lru():
-	pass
+def lru(queue):
+	# mark last page
+	last_page = 0
+	# index circulate
+	index = 0
+	# get numbers frames
+	frames = [-1 for x in range(queue[0])]
+	queue.pop(0)
+	# dictionary
+	table = {}
+	# swap pages
+	count = 0
+	# iterable in queue
+	for q in queue:
+		# check if the page is table
+		if not q in table:
+			table[q] = 1
+		else:
+			table[q] += 1 
+
+		# check if the page is out
+		if not q in frames:
+			if len(frames) != index:
+				frames[index] = q 
+				index += 1
+			else:
+				key, less = frames[0], table[frames[0]]
+				for i in range(len(frames)):
+					if less > table[frames[i]] and frames[i] != last_page:
+						key, less = frames[i], table[frames[i]]
+				frames[frames.index(key)] = q
+			count += 1
+			last_page = q
+	print("LRU", count)
 
 # Great Algorithm
 def otm():
 	pass
+
+# tests 
+l = [4,1,2,3,4,1,2,5,1,2,3,4,5] 
+l1 = [3,7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1]
+
+fifo(list(l))
+lru(list(l))
+fifo(list(l1))
+lru(list(l1))
